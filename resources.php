83fe7cd7bcfe0deaf45f9c172368fcd943d875cf
gacomhe <itsmejohnmer@gmail.com>
2017-07-08 22:00:07 +0800

    Added resources for materialize purposes

---
+++ resources.php
@@ -0,0 +1,10 @@
+<?php
+	echo '
+	<link rel="stylesheet" type="text/css" href="css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="css/icon.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+	<script type="text/javascript" src="js/custom.js"></script>
+	';
+?>
